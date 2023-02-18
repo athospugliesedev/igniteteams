b4ba1afea53f75d73fd53f5c3ce58ffa216a9503
athospugliesedev <athospugliesedev@gmail.com>
2023-02-18 18:13:02 -0300

    css in js configs

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
-import Groups from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Groups } from '@screens/Groups';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+  
+
+
   return (
-      <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
+    </ThemeProvider>
+      
   );
 }
